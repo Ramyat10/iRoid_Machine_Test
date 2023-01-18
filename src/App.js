a027d3866fd0476d120cfed6d5e36e792f1004c4
Ramyat10 <108926968+Ramyat10@users.noreply.github.com>
2023-01-18 15:01:59 +0530

    Add files via upload
    Added SRC folder

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/header';
+import Content from './component/content';
+import Footer from './component/footer';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Content />
+        <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default App;
